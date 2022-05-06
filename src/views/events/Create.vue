ad5a3ca312d914561b94b300631870c24d235e9b
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-05-06 10:42:26 +0100

    Events index view

---
+++ src/views/events/Create.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'OrganisationEventCreateView',
+  };
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/events/Show.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'OrganisationEventShowView',
+  };
+</script>
+
+<style lang="scss" scoped></style>
